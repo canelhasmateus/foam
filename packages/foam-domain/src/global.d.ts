6a2bcce9f4037f979c4f620010ef6ef7fed183bb
Mateus Canelhas <mateus.canelhas@revolut.com>
2023-01-08 11:40:45 -0300

    tmp

--- packages/foam-domain/src/global.d.ts
+++ packages/foam-domain/src/global.d.ts
@@ -1,3 +1,8 @@
+export declare const brand: unique symbol;
 
 export type Maybe<K> = K | null | undefined
-
+export type Update<K> = Maybe<Partial<K>>
+type Bag<T> = {
+	[ _: string ]: T;
+}
+export type Opaque<T> = T & { [ brand ]: T }
